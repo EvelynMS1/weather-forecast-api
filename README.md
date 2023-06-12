e400024cb4ebe113941760b987150fa8e6dc57c9
EvelynMS1 <102782127+EvelynMS1@users.noreply.github.com>
2023-06-11 18:19:12 -0700

    Display button and weather

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# weather-forecast-api
-Uses weather api to display current forecast of specified city -weather conditions, temperature, humidity, and wind speed. Allows to preview 5-day forecast. Searched cities are stored in search history.
+# weather-forecast-api  ![license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+Uses weather Api to display current forecast of specified city -weather conditions, temperature, humidity, and wind speed. Allows to preview 5-day forecast. Searched cities are stored in search history.
+
+
+## Description
+The motivation behind building this project was for the purpose of using a third party API, obtaining specific data provided by the API fetch and integrating it into the application. I built this project so that I may be able to view the current weather and up to a five day projection of the specified city. The problem that this project solves is that you can have a display of the weather for cities and with the use of local storage, conviniently searching for cities already visited. I learned the importance of understanding the data structure of the fetched api in order to obtain it correctly.
+    
+
+
+
+## Table of Contents (Optional)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [license](#license)
+- [Badges](#badges)
+- [Features](#features)
+- [How-to-Contribute](#how-to-contribute)
+- [Test](#test)
+- [Questions](#questions)
+## Installation
+
+N/A
+## Usage
+![Weather App](/weather.jpg)  
+
+
+
+## Credits
+
+N/A
+
+## License
+MIT
+
+## Badges
+
+N/A
+## Features
+
+N/A
+
+## How to Contribute
+
+N/A
+
+## Tests
+
+N/A
+## Questions
+-[Github](https://github.com/EvelynMS1)
+
+-montessobalevelyn@gmail.com
---
+++ javascript.js
@@ -0,0 +1,186 @@
+
+//will use the input of user 
+//retreving classes and id from html sheet 
+const $firstWeatherDay = document.getElementById("mainweather");
+const $fiveminiday = document.getElementById("miniWeather");
+const city = document.getElementById("cityname");
+const cityForm = document.getElementById("cityForm");
+const submitBtn = document.getElementById("submitbtn");
+const storedcityDiv = document.getElementById("storedCities");
+//array value if exists being assigned as valuesArray and if no value in local storage then the array item is retrieved and set as valuesArray
+let valuesArray = JSON.parse(localStorage.getItem("valuesArray")) || [];
+
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault(); // Prevent the form from submitting
+  
+  // Get the value of the submit button
+   var cityName = city.value; 
+
+  if(cityName){
+
+    const lowerCaseValuesArray = valuesArray.map(item => item.toLowerCase());
+
+    const checkcity =lowerCaseValuesArray.includes(cityName.toLowerCase());  
+ 
+    
+    console.log(checkcity);
+    if(checkcity === true){
+      geocity(cityName);
+    }else{
+     valuesArray.push(cityName); 
+      localStorage.setItem("valuesArray", JSON.stringify(valuesArray)); 
+      console.log(`Submit value: ${cityName}`);
+      geocity(cityName);
+// localStorage.setItem("cityData", JSON.stringify(cityName));
+       cityForm.reset();
+        
+      //   valuesArray.forEach(element => {
+      //   const searchedCitiesbtn = document.createElement("button"); 
+      //   searchedCitiesbtn.setAttribute('id','citybtn');
+      //   searchedCitiesbtn.textContent = element;
+      //   storedcityDiv.append(searchedCitiesbtn);
+      // });
+       const cityButton = document.createElement("button");
+       
+       cityButton.textContent = cityName;
+       cityButton.value = cityName;
+       cityButton.addEventListener("click", function(event) {
+       geocity(event.target.value);
+  });
+       cityButton.classList.add('city-button');  // Add a class for styling if needed
+       storedcityDiv.append(cityButton);
+//for each value of the valuesArray from local storage make btn and append value
+   
+       
+    
+    }
+// .join(",");;
+  } else{
+    console.log('no city inserted')
+  }
+    
+});
+
+
+
+// const cityName = document.getElementById("cityname").value;
+//when submit click event
+
+function geocity(citySpecified){
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${citySpecified}&limit=1&appid=f879b58236b7e1c5940e21d6ece79cf2`)
+//RESPONSE OBJECT converting to json
+.then((res)=>res.json())
+//json data response readable format
+.then((data)=>{
+    //statement manipulate data
+    console.log(data);
+    //data is array dot notation for properties
+    const lat = data[0].lat;
+    const long = data[0].lon;
+    cityweather(lat,long);
+})
+}
+
+
+//second function for weather forecast
+function cityweather(latitude,longitude){
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=f879b58236b7e1c5940e21d6ece79cf2&units=imperial`)
+    .then((res)=>res.json())
+    .then((data)=>{
+        console.log(data);
+         weatherdisplay(data);
+    })
+}
+function weatherdisplay(data){
+  $fiveminiday.innerHTML = '';
+  
+    const weatherobj = data;
+    //0 be a variable so that it is iteratable bc the following calls will be for each an every array number
+    //methods that can be used is for loops only loop through certain numbers of the array 
+//for every item in array call function weatherProperties
+  //every 7 day items in array//0,6,14,22,30,38 
+  const weatherdays = [weatherobj.list[13],weatherobj.list[22],weatherobj.list[30],weatherobj.list[38],weatherobj.list[39]];
+  //for loop through weatherdays calling function on each item to generate html page
+  for(day of weatherdays){
+    //call box creation function 
+   weatherProperties(day);
+  }
+  propertiesOfdata(weatherobj.list[0])
+  
+}
+//create 0 object property weather
+function weatherProperties(data){
+    const listobj = data;
+    const date1 = listobj.dt_txt;
+    const icon = listobj.weather[0].icon;
+    const temperature = listobj.main.temp;
+    const humidity = listobj.main.humidity;
+    const windspeed = listobj.wind.speed;
+    const objvalues = [temperature,windspeed,humidity];
+    //create html elements to add to div
+    //newly created div must be styled iinline block 
+
+   const $miniday = document.createElement("div");
+   const $datedata = document.createElement("p");
+   $datedata.textContent = date1;
+   $miniday.append($datedata);
+   $fiveminiday.append($miniday);
+      var $icondata = document.createElement("img");
+      $icondata.src = "https://openweathermap.org/img/w/" + icon + ".png";
+     $miniday.append($icondata);
+     
+     $fiveminiday.append($miniday);
+    // $minidata.textContent = date1;
+    // $minidata.textContent = icon;
+    // $minidata.textContent = temperature;    
+    // $minidata.textContent = windspeed;
+    for (var i = 0; i < objvalues.length; i++) { 
+      var $minidata = document.createElement("p");
+       $minidata.textContent = objvalues[i];
+       $miniday.append($minidata);
+       $miniday.classList.add('smalldivs');
+       $fiveminiday.append($miniday);
+  }
+  
+  
+    
+
+    console.log(windspeed);
+    console.log(temperature);
+    console.log(humidity);
+    console.log(icon);
+    console.log(listobj);
+    console.log(date1);
+}
+//add properties to a box
+// function boxCreation{
+//add to weatherdiv display 
+function propertiesOfdata(data){  
+     $firstWeatherDay.innerHTML ='';
+    const listobj = data;
+    const date1 = listobj.dt_txt;
+    const icon = listobj.weather[0].icon;
+    const temperature = listobj.main.temp;
+    const humidity = listobj.main.humidity;
+    const windspeed = listobj.wind.speed;
+    const objvalues = [temperature,windspeed,humidity];  
+    
+    const $miniday = document.createElement("div");
+   const $datedata = document.createElement("p");
+   $datedata.textContent = date1;
+   $miniday.append($datedata);
+   $firstWeatherDay.append($miniday);
+      var $icondata = document.createElement("img");
+      $icondata.src = "https://openweathermap.org/img/w/" + icon + ".png";
+     $miniday.append($icondata);
+     
+     $firstWeatherDay.append($miniday);
+     for (var i = 0; i < objvalues.length; i++) { 
+      var $minidata = document.createElement("p");
+       $minidata.textContent = objvalues[i];
+       $miniday.append($minidata);
+       $miniday.classList.add('oneweatherdivs');
+       $firstWeatherDay.append($miniday);
+  }
+}
+
